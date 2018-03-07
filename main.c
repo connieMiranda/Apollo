5d606943b8e1cf299fa99d9998a5efe72530414e
connieMiranda <coch5233@colorado.edu>
2018-03-07 10:50:08 -0700

    man.c at end of class 03/07

--- main.c
+++ main.c
@@ -8,15 +8,28 @@
 
 /*--------Global Variables---------*/
 
+double G=6.67408e-11;
+double mM=7.34767309e22;
+double mE=5.97219e24;
+double mS=28833;
+double rM=1737100;
+double rE=6371000;
 
 
+double dEM=384403000;
+double thetaM=42.5;
 
+double dES=338000000;
+double thetaS=50;
 
+double locM=
+double locS=
+double locE=
 
 /*---------------------------------*/
 
 /*--------Sub Functions------------*/
-double
+double calcF() /*Calculates forces between all three bodies*/
 
 
 

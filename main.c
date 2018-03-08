e8b2d1327d5bd1db4c777fe740a534a061f8863a
connieMiranda <coch5233@colorado.edu>
2018-03-08 12:28:00 -0700

    Added more while I had free time

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include<stdio.h>
 #include<time.h>
 
-
+#define PI=3.141592;
+#define deg2rad=PI/180;
 
 /*---------------------------------*/
 
@@ -22,9 +23,12 @@ double thetaM=42.5;
 double dES=338000000;
 double thetaS=50;
 
-double locM=
-double locS=
-double locE=
+double locM[2]={dEM*cos(thetaM*deg2rad),dEM*sin(thetaM*deg2rad)};
+double locS[2]={dES*cos(thetaS*deg2rad),dEM*sin(thetaS*deg2rad)};
+double locE[2]={0,0};
+
+double vM=(G*mE*mE)/((mE+mM)+dEM);
+double velM[2]={-vM*sin(thetaM*deg2rad),vM*cos(thetaM*deg2rad)};
 
 /*---------------------------------*/
 
